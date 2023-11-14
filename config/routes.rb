3dbf1e159a27d9b1e81863407cd88cf49160ac89
Eddie Gutierrez <eddygutierrez175@gmail.com>
2023-11-14 15:16:03 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  #get 'home/index'
+  get 'home/about'
+  root 'friends#index'
+  #root 'home#index'
+
+  #get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
